84179ec2e23b418c6b751214cd3db33c1bde4a9c
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2025-07-18 15:50:22 +0200

    cypress test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,6 +1,6 @@
 describe('Login spec', () => {
   it('Login successfull', () => {
-    cy.visit('/login')
+    cy.visit('/login');
 
     cy.intercept('POST', '/api/auth/login', {
       body: {
@@ -8,20 +8,43 @@ describe('Login spec', () => {
         username: 'userName',
         firstName: 'firstName',
         lastName: 'lastName',
-        admin: true
+        admin: true,
       },
-    })
+    });
 
     cy.intercept(
       {
         method: 'GET',
         url: '/api/session',
       },
-      []).as('session')
+      []
+    ).as('session');
 
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.get('input[formControlName=email]').type('yoga@studio.com');
+    cy.get('input[formControlName=password]').type(
+      `${'test!1234'}{enter}{enter}`
+    );
 
-    cy.url().should('include', '/sessions')
-  })
+    cy.url().should('include', '/sessions');
+  });
+
+  it('Login unsuccessful', () => {
+    cy.visit('/login');
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401,
+      body: {},
+    }).as('failedLogin');
+
+    cy.get('input[formControlName=email]').type('wrong@studio.com');
+    cy.get('input[formControlName=password]').type(
+      'wrongpassword{enter}{enter}'
+    );
+
+    cy.wait('@failedLogin');
+
+    cy.contains('An error occurred').should('be.visible');
+
+    cy.url().should('include', '/login');
+  });
 });
